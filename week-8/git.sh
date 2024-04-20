90b5eebb55c0102c9c694ee251232b62e719b483
Revanth422113 <167018650+Revanth422113@users.noreply.github.com>
2024-04-20 20:28:24 +0530

    Add files via upload

---
+++ week-8/git.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/Revanth422113/lab
+
+git status
+
+cd lab
+
+git status
+
+git checkout -b b1
+
+git status
+
+git status
+
+git add .
+
+git status
+
+git commit -m "422113-lab"
+
+git push origin b1
